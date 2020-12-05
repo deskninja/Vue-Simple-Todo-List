3b2882148bbbe74b86059ee46c52476a9a15cba1
Joshua Wells <joshuawells109@gmail.com>
2020-12-05 00:14:41 -0700

    now deletes completed tasks and uses and if else to show a message when there are no tasks.

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,39 @@
             {{ name }}'s To Do List
         </h4>
         <div class="container-fluid p-4">
-            <div class="row">
-                <div class="col font-weight-bold">Task</div>
-                <div class="col-2 font-weight-bold">Done</div>
-            </div>
-            <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
-                <div class="col">{{ t.action }}</div>
-                <div class="col-2 text-center">
-                    <input type="checkbox" v-model="t.done" class="form-check-input" />
-                    {{ t.done }}
+            <div class="row" v-if="filteredTasks.length == 0">
+                <div class="col text-center">
+                    <h5>
+                        Nothing To Do! Take a nap!
+                    </h5>
                 </div>
+            </div>
+            <template v-else>
+                <div class="row">
+                    <div class="col font-weight-bold">Task</div>
+                    <div class="col-2 font-weight-bold">Done</div>
+                </div>
+                <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+                    <div class="col">{{ t.action }}</div>
+                    <div class="col-2 text-center">
+                        <input type="checkbox" v-model="t.done" class="form-check-input" />
+                        {{ t.done }}
+                    </div>
+                </div>
+            </template>
+            <div class="row py-2">
+                <div>
+                    <input v-model="newItemText" class="form-control" />
+                </div>
+                <div>
+                    <button class="btn btn-primary" v-on:click="addNewTodo">Add</button>
+                </div>
+            </div>
+            <div class="col text-center">
+                <button class="btn btn-md btn-warning" 
+                        v-on:click="deleteCompleted" >
+                        Delete Completed>
+                </button>
             </div>
             <div class="row bg-secondary py-2 mt-2 text-white">
                 <div class="col text-center">
@@ -21,14 +44,6 @@
                     <label class="form-check-label font-weight-bold">
                         Hide completed tasks
                     </label>
-                </div>
-            </div>
-            <div class="row py-2">
-                <div>
-                    <input v-model="newItemText" class="form-control"/>
-                </div>
-                <div>
-                    <button class="btn btn-primary" v-on:click="addNewTodo">Add</button>
                 </div>
             </div>
         </div>
@@ -59,8 +74,15 @@
                     action: this.newItemText,
                     done: false
                 })
-                localStorage.setItem('todos', JSON.stringify(this.tasks));
+                this.storeData
                 this.newTextItem = '';
+            },
+            storeData() {
+                localStorage.setItem('todos', JSON.stringify(this.tasks));
+            },
+            deleteCompleted() { //deletes completed tasks
+                this.tasks = this.tasks.filter(t => !t.done);
+                this.storeData();
             }
         },
         created() {
